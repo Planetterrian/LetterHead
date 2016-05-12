366889ca5b9f4a23a4677709d79e9ab4acb7b69f
petesimard56 <petesimard56@gmail.com>
2016-05-12 13:14:40 +0700

    Added server project

---
+++ Server/LetterHeadServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LetterHeadServer.Startup))]
+namespace LetterHeadServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
