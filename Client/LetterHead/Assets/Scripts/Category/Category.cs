91ac1c798986d0f2ae9493ef4fa6a831826d447b
Pete Simard <petesimard56@gmail.com>
2016-05-16 17:10:23 +0700

    Categories

---
+++ Client/LetterHead/Assets/Scripts/Category/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class Category
+{
+    public string name;
+    public string description;
+
+    public Func<string, int> getWordScore;
+
+}
