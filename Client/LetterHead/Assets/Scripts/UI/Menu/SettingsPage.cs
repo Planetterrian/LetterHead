e840f37e5c7f512d31ceb6fe15b93b991d1192af
Pete Simard <petesimard56@gmail.com>
2016-05-24 13:57:24 +0700

    Settings

---
+++ Client/LetterHead/Assets/Scripts/UI/Menu/SettingsPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UI.Pagination;
+using UnityEngine;
+
+public class SettingsPage : Page
+{
+     
+
+    private void Awake()
+    {
+    }
+
+    private void Start()
+    {
+    }
+}
