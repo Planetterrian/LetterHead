4b8f8ad4f412881148b1ece92308701dff8e04e3
petesimard56 <petesimard56@gmail.com>
2016-05-24 16:32:17 +0700

    Facebook

---
+++ Client/LetterHead/Assets/Scripts/3rdParty/FacebookManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Facebook.Unity;
+using UnityEngine;
+
+class FacebookManager : Singleton<FacebookManager>
+{
+    void Start()
+    {
+        FB.Init(() =>
+        {
+            Debug.Log("Facebook Initilized");
+        });
+    }
+}
--- Client/LetterHead/Assets/Scripts/UI/Menu/LoginScene.cs
+++ Client/LetterHead/Assets/Scripts/UI/Menu/LoginScene.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Facebook.Unity;
 using UnityEngine;
 
 public class LoginScene : GuiScene
@@ -25,7 +26,12 @@ public class LoginScene : GuiScene
 
     public void OnLoginFacebookClicked()
     {
-
+        FB.LogInWithReadPermissions(new List<string>() {"friends"}, FacebookLoginCallback);
     }
 
+    private void FacebookLoginCallback(ILoginResult result)
+    {
+        Debug.Log(result.Error);
+        Debug.Log(result.AccessToken.TokenString);
+    }
 }
