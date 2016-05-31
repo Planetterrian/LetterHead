2799f9656e6992371ae6381075e4d1cea610cc75
petesimard56 <petesimard56@gmail.com>
2016-05-31 16:03:36 +0700

    facebook connect button

--- Client/LetterHead/Assets/Scripts/UI/Menu/SettingsPage.cs
+++ Client/LetterHead/Assets/Scripts/UI/Menu/SettingsPage.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Facebook.Unity;
+using Newtonsoft.Json;
 using UI.Pagination;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,6 +17,8 @@ public class SettingsPage : Page
     public GameObject linkFacebookButton;
     public GameObject unLinkFacebookButton;
 
+    public Button premiumUpgradeButton;
+
     private void Awake()
     {
     }
@@ -27,6 +31,8 @@ public class SettingsPage : Page
     {
         linkFacebookButton.SetActive(string.IsNullOrEmpty(ClientManager.Instance.myUserInfo.FacebookPictureUrl));
         unLinkFacebookButton.SetActive(!string.IsNullOrEmpty(ClientManager.Instance.myUserInfo.FacebookPictureUrl));
+
+        premiumUpgradeButton.gameObject.SetActive(false);
 
         musicEnabledToggle.isOn = PersistManager.Instance.MusicEnabled;
         clearWordToggle.isOn = PersistManager.Instance.ClearWord;
@@ -76,12 +82,45 @@ public class SettingsPage : Page
 
     public void DisconnectFacebookClicked()
     {
-
+        Srv.Instance.POST("User/FacebookDisconnect", null, s =>
+        {
+            ClientManager.Instance.myUserInfo.FacebookPictureUrl = "";
+            Refresh();
+        });
     }
 
     public void CconnectFacebookClicked()
     {
+        FB.LogInWithReadPermissions(new List<string>() { "user_friends" }, FacebookLoginCallback);
+    }
 
+    private void FacebookLoginCallback(ILoginResult result)
+    {
+        if (result.Cancelled)
+        {
+            DialogWindowTM.Instance.Hide();
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(result.Error))
+        {
+            DialogWindowTM.Instance.Error(result.Error);
+            return;
+        }
+
+        DialogWindowTM.Instance.Show("Facebook", "We're processing your Login... Please wait.", () => { }, () => { }, "");
+
+        Srv.Instance.POST("User/FacebookConnect", new Dictionary<string, string>() { { "token", result.AccessToken.TokenString } }, (s) =>
+        {
+            ClientManager.Instance.RefreshMyInfo(false, b =>
+            {
+                DialogWindowTM.Instance.Hide();
+                Refresh();
+            });
+        }, s =>
+        {
+            DialogWindowTM.Instance.Error(s);
+        });
     }
 
     public void AboutClicked()
