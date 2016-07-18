23d45ac07a465478dc9289df1e88ab12d549f707
Pete Simard <petesimard56@gmail.com>
2016-07-18 10:45:22 +0700

    Auto match reload

--- Client/LetterHead/Assets/Scripts/Game/GameManager.cs
+++ Client/LetterHead/Assets/Scripts/Game/GameManager.cs
@@ -77,6 +77,9 @@ public abstract class GameManager : Singleton<GameManager>
             }
             else
                 onLoadOverrideAction();
+        }, s =>
+        {
+            LoadMatchDetails(onLoadOverrideAction);
         });
     }
 
