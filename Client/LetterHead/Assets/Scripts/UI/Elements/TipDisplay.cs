31961d6cad489d323967ce761f93c3ec312da6fd
Pete Simard <petesimard56@gmail.com>
2016-08-06 16:30:06 +0700

    Tips

---
+++ Client/LetterHead/Assets/Scripts/UI/Elements/TipDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class TipDisplay : MonoBehaviour
+{
+    public string[] tips;
+
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        text.text = "Tip: " + tips[UnityEngine.Random.Range(0, tips.Length)];
+    }
+}
