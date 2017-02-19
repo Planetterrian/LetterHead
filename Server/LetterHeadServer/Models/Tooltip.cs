00d34162933b6a3fc39aadbda139cead8f4fff4f
Pete Simard <petesimard56@gmail.com>
2017-02-19 13:03:55 +0700

    Tooltip fix

--- Server/LetterHeadServer/Models/Tooltip.cs
+++ Server/LetterHeadServer/Models/Tooltip.cs
@@ -7,7 +7,13 @@ namespace LetterHeadServer.Models
 {
     public class Tooltip
     {
+        public Tooltip()
+        {
+            Active = true;
+        }
+
         public int Id { get; set; }
         public string Content { get; set; }
+        public bool Active { get; set; }
     }
 }
